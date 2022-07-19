54f599276fa580d25b82c0ddfb5d1ee5e866f5bf
Lydia <lydia.haway@gmail.com>
2022-07-19 15:58:58 +0200

    ex

--- src/public/exercise_5_public.php
+++ src/public/exercise_5_public.php
@@ -7,6 +7,72 @@ declare(strict_types=1);
 Copy the class of exercise 1.
 
 TODO: Change the properties to private.
-TODO: Fix the errors without using getter and setter functions.
+TODO: Fix the errors by using getter and setter functions.
 TODO: Change the price to 3.5 euro and print it also on the screen on a new line.
 */
+
+class Bevrage
+{
+
+    private $color;
+    private $price;
+    private $temperature;
+
+
+    public function __construct($color, $price, $temperature)
+    {
+
+        $this->color = $color;
+
+        if (!is_float($price)) {
+            exit;
+        } else {
+            $this->price = $price . " €";
+        }
+
+
+        if (!empty($temperature)) {
+            $this->temperature = $temperature;
+        } else {
+
+            $this->temperature = "cold";
+        }
+    }
+
+    public function getinfo()
+    {
+        return "This bevrage is $this->temperature and $this->color";
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price . " €";
+    }
+
+    public function getPrice(): string
+    {
+        return $this->price;
+    }
+
+    public function setTemperature($temperature)
+    {
+        $this->temperature = $temperature;
+    }
+
+    public function getTemperature()
+    {
+        return $this->temperature;
+    }
+}
+
+$cola = new Bevrage("black", 2.0, "");
+
+echo $cola->getinfo();
+
+echo "<br>" . $cola->getTemperature();
+
+echo "<br>" . $cola->getPrice();
+
+$cola->setPrice(3.5);
+
+echo "<br>" . $cola->getPrice();
--- src/public/exercise_6_const.php
+++ src/public/exercise_6_const.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Bevrage as GlobalBevrage;
+
 /* EXERCISE 6
 
 Copy the classes of exercise 2.
@@ -16,3 +18,137 @@ TODO: Make sure that every print is on a new line.
 
 Use typehinting everywhere!
 */
+
+class Bevrage
+{
+
+    private $color;
+    private $price;
+    private $temperature;
+    const barman = "Het Vervolg";
+
+
+    public function __construct($color, $price, $temperature)
+    {
+
+        $this->color = $color;
+
+        if (!is_float($price)) {
+            exit;
+        } else {
+            $this->price = $price . " €";
+        }
+
+
+        if (!empty($temperature)) {
+            $this->temperature = $temperature;
+        } else {
+
+            $this->temperature = "cold";
+        }
+    }
+
+    //_______________
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price . " €";
+    }
+
+    public function getPrice(): string
+    {
+        return $this->price;
+    }
+
+    public function setTemperature($temperature)
+    {
+        $this->temperature = $temperature;
+    }
+
+    public function getTemperature()
+    {
+        return $this->temperature;
+    }
+
+    //______________________
+
+    public function getinfo(): string
+    {
+        return "This bevrage is $this->temperature and $this->color";
+    }
+}
+
+class Beer extends Bevrage
+{
+    private $name;
+    private $alcoholPercentage;
+
+    public function __construct($color, $price, $temperature, $name, $alcoholPercentage)
+    {
+        parent::__construct($color, $price, $temperature);
+
+        $this->name = $name;
+
+        if (!is_float($alcoholPercentage)) {
+            exit;
+        } else {
+            $this->alcoholPercentage = $alcoholPercentage . " %";
+        }
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setAlcoholPercentage($alcoholPercentage)
+    {
+        $this->alcoholPercentage = $alcoholPercentage . " %";
+    }
+
+    public function getAlcoholPercentage(): string
+    {
+        return $this->alcoholPercentage;
+    }
+}
+
+$cola = new Bevrage("black", 2.0, "");
+
+echo $cola->getinfo();
+
+echo "<br>" . $cola->getTemperature();
+
+echo "<br>" . $cola->getPrice() . "<br>";
+
+$Duvel = new Beer("blond", 3.5, "", "Duvel", 8.5);
+
+echo "<br>" . $Duvel->getColor() . "<br>";
+
+echo $Duvel->getinfo();
+
+echo "<br>" . $Duvel->getName();
+
+echo "<br>" . $Duvel->getAlcoholPercentage();
+
+$myclass = "Bevrage";
+
+echo "<br>";
+
+echo "<br>";
+
+echo $myclass::barman;
